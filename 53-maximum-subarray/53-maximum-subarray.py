85c8eb98b53dd170a6688ee46143feef74cb0b8b
David Archuleta <darchuletajr@gmail.com>
2022-07-10 14:58:59 -0600

    Time: 3528 ms (5.02%), Space: 28 MB (39.21%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,22 +1,35 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        """
-        [-2,1,-3,4,-1,2,1,-5,4] => 6
-        [1] => 1
-        [5,4,-1,7,8] => 23
-        [] => throw error
-        [1] => 1
-        [1,1] = > 2
-        [-10_000, 2] => 2
-        [999, 999, -10_000] => 2000
+        def findBestSubarray(nums, left, right):
+            # Base case - empty array.
+            if left > right:
+                return -math.inf
+
+            mid = (left + right) // 2
+            curr = best_left_sum = best_right_sum = 0
+
+            # Iterate from the middle to the beginning.
+            for i in range(mid - 1, left - 1, -1):
+                curr += nums[i]
+                best_left_sum = max(best_left_sum, curr)
+
+            # Reset curr and iterate from the middle to the end.
+            curr = 0
+            for i in range(mid + 1, right + 1):
+                curr += nums[i]
+                best_right_sum = max(best_right_sum, curr)
+
+            # The best_combined_sum uses the middle element and
+            # the best possible sum from each half.
+            best_combined_sum = nums[mid] + best_left_sum + best_right_sum
+
+            # Find the best subarray possible from both halves.
+            left_half = findBestSubarray(nums, left, mid - 1)
+            right_half = findBestSubarray(nums, mid + 1, right)
+
+            # The largest of the 3 is the answer for any given input array.
+            return max(best_combined_sum, left_half, right_half)
         
-        """
-        # initialize our variables with the first element
-        current_subarray = max_subarray = nums[0]
-        # start with 2nd element since we already used the first one
-        for num in nums[1:]:
-            # if current_subarray is negative, throw it away
-            # otherwise, keep adding to it
-            current_subarray = max(num, current_subarray + num)
-            max_subarray = max(max_subarray, current_subarray)
-        return max_subarray
+        # Our helper function is designed to solve this problem for
+        # any array - so just call it using the entire input!
+        return findBestSubarray(nums, 0, len(nums) - 1)
