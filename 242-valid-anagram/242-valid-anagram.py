a84ce71f35679030ee5e744df788db882873ac0e
David Archuleta <darchuletajr@gmail.com>
2022-07-16 12:17:35 -0600

    Time: 47 ms (94.41%), Space: 14.4 MB (66.69%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,13 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        
-        table = [0] * 26 # alphabet is limited to 26 chars
-        for char in s:
-            table[ord(char) - ord('a')] += 1
-        for char in t:
-            table[ord(char) - ord('a')] -= 1
-            if table[ord(char) - ord('a')] < 0:
-                return False
-        return True
+        return collections.Counter(s) == collections.Counter(t)
