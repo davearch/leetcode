2f2e3415a31d6cdc9ab1672bc9d8a454c9b10fda
David Archuleta <darchuletajr@gmail.com>
2022-07-18 12:24:00 -0600

    Time: 210 ms (22.71%), Space: 20.9 MB (29.70%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,6 +1,14 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
+        def pop_count(x: int) -> int:
+            count = 0
+            while x != 0:
+                x &= x - 1
+                count += 1
+            return count
+        
         ans = [0] * (n + 1)
-        for x in range(1, n + 1):
-            ans[x] = ans[x & (x - 1)] + 1
-        return ans 
+        for x in range(n + 1):
+            ans[x] = pop_count(x)
+        
+        return ans
