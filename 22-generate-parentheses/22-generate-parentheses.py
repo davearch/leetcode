cbf769299d99a38cac6a7eb082e601c244ac4b23
David Archuleta <darchuletajr@gmail.com>
2022-08-13 20:07:02 -0600

    Time: 36 ms (93.10%), Space: 14.3 MB (40.06%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -0,0 +1,6 @@
+class Solution:
+    def generateParenthesis(self, n, open=0):
+        if n > 0 <= open:
+            return ['(' + p for p in self.generateParenthesis(n-1, open+1)] + \
+                   [')' + p for p in self.generateParenthesis(n, open-1)]
+        return [')' * open] * (not n)
