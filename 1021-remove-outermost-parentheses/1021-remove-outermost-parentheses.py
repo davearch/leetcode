09b2117ec1307b4ec5aee68e86a57e7c926188d0
David Archuleta <darchuletajr@gmail.com>
2022-08-21 17:25:20 -0600

    Time: 69 ms (32.21%), Space: 13.8 MB (99.60%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
@@ -1,6 +1,7 @@
 class Solution:
     def removeOuterParentheses(self, s: str) -> str:
-        res, opened = [], 0
+        res = []
+        opened = 0
         for c in s:
             if c == '(' and opened > 0: res.append(c)
             if c == ')' and opened > 1: res.append(c)
