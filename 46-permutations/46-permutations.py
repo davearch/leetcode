d89c2504279f1d645eea9f2aaa3d8d2bef44f775
David Archuleta <darchuletajr@gmail.com>
2022-09-05 12:21:06 -0600

    Time: 79 ms (17.99%), Space: 14.1 MB (22.96%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,11 +1,6 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        if not nums:
-            return []
-        if len(nums) == 1:
-            return [nums]
         n = len(nums)
-        
         def backtrack(first = 0):
             if first == n:
                 result.append(nums[:])
@@ -14,7 +9,6 @@ class Solution:
                 nums[i], nums[first] = nums[first], nums[i]
                 backtrack(first+1)
                 nums[i], nums[first] = nums[first], nums[i]
-        
         result = []
         backtrack()
         return result
