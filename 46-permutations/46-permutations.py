55231b896ba6be7fd276416e81a09169e92eb5e3
David Archuleta <darchuletajr@gmail.com>
2022-09-05 13:12:12 -0600

    Time: 66 ms (44.30%), Space: 14.2 MB (22.96%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,14 +1,18 @@
+from collections import deque
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
-        def backtrack(first = 0):
-            if first == len(nums):
-                result.append(nums[:])
-                return
-            for i in range(first, n):
-                nums[i],nums[first] = nums[first],nums[i]
-                backtrack(first+1)
-                nums[i],nums[first] = nums[first],nums[i]
+        q = deque([])
+        q.append([])
         result = []
-        backtrack()
+        for num in nums:
+            n = len(q)
+            for _ in range(n):
+                old_permutation = q.popleft()
+                for i in range(len(old_permutation)+1):
+                    new_permutation = list(old_permutation)
+                    new_permutation.insert(i, num)
+                    if len(new_permutation) == len(nums):
+                        result.append(new_permutation)
+                    else:
+                        q.append(new_permutation)
         return result
