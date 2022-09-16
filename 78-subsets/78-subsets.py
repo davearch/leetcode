1a0c656c621d9573aa5cc4e976cdedb2452b612b
David Archuleta <darchuletajr@gmail.com>
2022-09-16 06:27:56 -0600

    Time: 68 ms (19.71%), Space: 14.1 MB (82.54%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,14 +1,13 @@
 from copy import deepcopy
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        result = []
+        results = []
         nums.sort()
-        self.backtrack(result, [], nums, 0)
-        return result
-    
-    def backtrack(self, result, templist, nums, start):
-        result.append(deepcopy(templist))
-        for i in range(start, len(nums)):
-            templist.append(nums[i])
-            self.backtrack(result, templist, nums, i+1)
-            templist.pop()
+        self.backtrack(nums, [], results, 0)
+        return results
+    def backtrack(self, nums, tempList, results, index):
+        results.append(deepcopy(tempList))
+        for i in range(index, len(nums)):
+            tempList.append(nums[i])
+            self.backtrack(nums, tempList, results, i+1)
+            tempList.pop()
