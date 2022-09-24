49d15cb224d3b725aec2938776e9b4e7bbbcd969
David Archuleta <darchuletajr@gmail.com>
2022-09-24 14:54:02 -0600

    Time: 191 ms (13.06%), Space: 14.3 MB (52.79%) - LeetHub

---
+++ 271-encode-and-decode-strings/271-encode-and-decode-strings.py
@@ -0,0 +1,19 @@
+class Codec:
+
+    def encode(self, strs):
+        return ''.join('%d:' % len(s) + s for s in strs)
+
+    def decode(self, s):
+        strs = []
+        i = 0
+        while i < len(s):
+            j = s.find(':', i)
+            i = j + 1 + int(s[i:j])
+            strs.append(s[j+1:i])
+        return strs
+        
+
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(strs))
