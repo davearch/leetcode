0776c873db1c55834282bae5b449d516b1884588
David Archuleta <darchuletajr@gmail.com>
2022-09-24 09:40:39 -0600

    Time: 261 ms (12.72%), Space: 17.2 MB (81.10%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.py
@@ -0,0 +1,35 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        """
+        ["eat","tea","tan","ate","nat","bat"]
+          ate,  bat,  eat,  tea,  tan,  nat
+          
+          {sortedWord: [word, word, word]}
+          
+          aet,  aet,  ant,  aet,  ant,  abt
+          {aet}
+        
+        O(1)
+        create hashmap of sortedWord to list of anagrams
+        O(n*mlogm)
+        for each word, compare the sorted version with a key in the map
+        O(1)
+        if the key exists, append the current word to it's list
+        O(1)
+        else, create new entry with word
+        O(n)
+        create an output array and loop through map appending each list
+        O(1)
+        return output array
+        """
+        sortedWordToListOfAnagrams = {}
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            if sorted_word in sortedWordToListOfAnagrams:
+                sortedWordToListOfAnagrams[sorted_word].append(word)
+            else:
+                sortedWordToListOfAnagrams[sorted_word] = [word]
+        output = []
+        for k, v in sortedWordToListOfAnagrams.items():
+            output.append(v)
+        return output
