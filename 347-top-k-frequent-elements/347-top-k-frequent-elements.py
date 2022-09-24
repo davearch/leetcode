1ace1b1b3bbe89384407088598a0860006f2b5e9
David Archuleta <darchuletajr@gmail.com>
2022-09-24 10:51:13 -0600

    Time: 192 ms (42.87%), Space: 18.6 MB (71.83%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,10 +1,12 @@
 from collections import Counter
-from heapq import heappush, nlargest
+from heapq import heappush, heappushpop
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         numCount = Counter(nums)
-                
         minheap = []
         for num, count in numCount.items():
-            heappush(minheap, (count, num))
-        return [pair[1] for pair in nlargest(k, minheap)]
+            if len(minheap) < k:
+                heappush(minheap, (count, num))
+            else:
+                heappushpop(minheap, (count, num))
+        return [pair[1] for pair in minheap]
