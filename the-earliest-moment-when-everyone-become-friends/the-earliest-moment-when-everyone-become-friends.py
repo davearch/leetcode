0d1aa009163ba29b405a063fd3f01f85473b537c
David Archuleta <darchuletajr@gmail.com>
2022-09-30 18:38:18 -0600

    Time: 116 ms, Memory: 14.2 MB - LeetHub

--- the-earliest-moment-when-everyone-become-friends/the-earliest-moment-when-everyone-become-friends.py
+++ the-earliest-moment-when-everyone-become-friends/the-earliest-moment-when-everyone-become-friends.py
@@ -1,15 +1,19 @@
 class Solution:
     def earliestAcq(self, logs: List[List[int]], n: int) -> int:
         logs.sort(key=lambda x: x[0])
+        
         f = list(range(n))
         r = [1 for _ in range(n)]
+        
         size = n
         min_time = float('inf')
+        
         def find(x):
             if f[x] == x:
                 return x
             f[x] = find(f[x])
             return f[x]
+        
         def union(a, b):
             if r[a] > r[b]:
                 f[b] = a
@@ -28,22 +32,3 @@ class Solution:
             if size == 1:
                 min_time = min(min_time, t)
         return min_time if size == 1 else -1
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
