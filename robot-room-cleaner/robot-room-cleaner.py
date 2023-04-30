3354fceb9d301cab6fc132ae5e0d52e479b4770d
David Archuleta Jr <darchuletajr@gmail.com>
2023-04-30 10:51:46 -0600

    Time: 76 ms, Memory: 17.8 MB - LeetHub

---
+++ robot-room-cleaner/robot-room-cleaner.py
@@ -0,0 +1,62 @@
+# """
+# This is the robot's control interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class Robot:
+#    def move(self):
+#        """
+#        Returns true if the cell in front is open and robot moves into the cell.
+#        Returns false if the cell in front is blocked and robot stays in the current cell.
+#        :rtype bool
+#        """
+#
+#    def turnLeft(self):
+#        """
+#        Robot will stay in the same cell after calling turnLeft/turnRight.
+#        Each turn will be 90 degrees.
+#        :rtype void
+#        """
+#
+#    def turnRight(self):
+#        """
+#        Robot will stay in the same cell after calling turnLeft/turnRight.
+#        Each turn will be 90 degrees.
+#        :rtype void
+#        """
+#
+#    def clean(self):
+#        """
+#        Clean the current cell.
+#        :rtype void
+#        """
+
+class Solution:
+    def cleanRoom(self, robot):
+        """
+        :type robot: Robot
+        :rtype: None
+        """
+        def go_back():
+            robot.turnRight()
+            robot.turnRight()
+            robot.move()
+            robot.turnRight()
+            robot.turnRight()
+            
+        def backtrack(cell = (0, 0), d = 0):
+            visited.add(cell)
+            robot.clean()
+            
+            for i in range(4):
+                new_dir = (d + i) % 4
+                new_cell = (cell[0] + directions[new_dir][0], \
+                            cell[1] + directions[new_dir][1])
+                if new_cell not in visited and robot.move():
+                    backtrack(new_cell, new_dir)
+                    go_back()
+                robot.turnRight()
+            
+        visited = set()
+        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        backtrack()
+        
